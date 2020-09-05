e79430f53240d14fd606b967eb0e6b7c3fc8e835
Artyom Kombarov <artyom.kombaroff@gmail.com>
2020-09-05 15:08:35 +0300

    createCityList function

--- js/weather.js
+++ js/weather.js
@@ -1,6 +1,24 @@
 "use strict";
 
-getRequest('city.list.json').then(resp => console.log(filterListByCountry(resp, 'RU')));
+//DOM Elements
+const selectCountries = document.querySelector('.country');
+const selectCyties = document.querySelector('.city');
+
+//Events
+selectCountries.addEventListener('input', () => selectCountry(selectCountries));
+selectCountry(selectCountries);
+
+function selectCountry(select) {
+	const selected = select.selectedIndex;
+	const country = select[selected].value;
+
+	getRequest('city.list.json')
+		.then(resp => filterListByCountry(resp, country))
+		.then(list => {
+			selectCyties.classList.remove('hidden');
+			selectCyties.innerHTML = createCityList(list);
+		})
+}
 
 async function getRequest(url) {
 
@@ -20,6 +38,22 @@ function filterListByCountry(list, country) {
 	return list.filter(city => {
 		return city.country === country;
 	})
+
+}
+
+function createCityList(list) {
+
+	let html = '';
+
+	list.forEach(city => {
+		const cityHtml = `
+		<option value=${city.id}>${city.name}</option>
+		`
+
+		html += cityHtml;
+	});
+
+	return html;
 
 }
 
