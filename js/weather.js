ec4a2ab4861315f297d63bae3b915f43b53f254b
Artyom Kombarov <artyom.kombaroff@gmail.com>
2020-09-05 13:56:54 +0300

    create filter function and getRequest

--- js/weather.js
+++ js/weather.js
@@ -1,2 +1,25 @@
 "use strict";
 
+getRequest('city.list.json').then(resp => console.log(filterListByCountry(resp, 'RU')));
+
+async function getRequest(url) {
+
+	const req = await fetch(url);
+
+	if (!req.ok) {
+		console.log('error!');
+		return;
+	}
+
+	return await req.json();
+
+}
+
+function filterListByCountry(list, country) {
+
+	return list.filter(city => {
+		return city.country === country;
+	})
+
+}
+
