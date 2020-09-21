927924e8dd6a229a9a226a6a85727d04623cfe67
Artyom Kombarov <artyom.kombaroff@gmail.com>
2020-09-21 15:33:16 +0300

    add toggle method

--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -1,7 +1,6 @@
 export default class Accordion {
   constructor(wrapper = '', {trigger = '', content = ''} = {}) {
     this.$wrappers = document.querySelectorAll(wrapper);
-    this.$triggers = document.querySelectorAll(trigger);
     this.trigger = trigger;
     this.content = content;
 
@@ -9,26 +8,25 @@ export default class Accordion {
   }
 
   setup() {
-    this.closeAll = this.closeAll.bind(this);
-
     this.$wrappers.forEach(item => {
       item.addEventListener('click', (e) => {
         const trigger = e.target.closest(this.trigger) ? e.target.closest(this.trigger) : '';
         const content = trigger.nextElementSibling;
+
         if (trigger) {
-          this.closeAll();
-          content.classList.toggle('opened');
-          content.style.maxHeight = (content.scrollHeight + 50) + 'px';
+          this.toggle(content);
         }
       })
     })
   }
 
-  closeAll() {
-    this.$triggers.forEach(item => {
-      const content = item.nextElementSibling;
+  toggle(content) {
+    if (content.classList.contains('opened')) {
       content.classList.remove('opened');
-      content.style.maxHeight = '';
-    })
+      content.style.maxHeight = '0px';
+    } else {
+      content.classList.add('opened');
+      content.style.maxHeight = (content.scrollHeight + 50) + 'px';
+    }
   }
 }
